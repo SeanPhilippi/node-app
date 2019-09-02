82d3f208d35cb233f7ea7eac8bf8e97a952e019c
Sean Philippi <sean.philippi@gmail.com>
2019-09-02 10:52:50 -0500

    exercise 2 complete

---
+++ exercise2/exercise2.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use('/users', (req, res, next) => {
+  // console.log('in 2nd middleware, res is returned here');
+  res.send(`<p>message from response in /users</p>`)
+});
+
+app.use('/', (req, res, next) => {
+  // console.log('in 1st middleware, res always hits this');
+  res.send(`<p>message from response in /</p>`)
+  // next();
+});
+
+app.listen(3004, () => console.log('listening on 3004, exercise 2'));
