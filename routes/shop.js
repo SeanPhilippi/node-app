6e0c88fcb73170f5d24dff72bec7b2a6cbd0c569
Sean Philippi <sean.philippi@gmail.com>
2019-09-03 19:34:35 -0500

    mapping products dynamically using pug

--- routes/shop.js
+++ routes/shop.js
@@ -7,9 +7,11 @@ const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
   console.log('products', adminData.products);
+  const products = adminData.products;
   // path.join() is used vs concat because it detects the operating system the app is running on and adjusts for '/' or '\'
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop');
+  // now using pugfile since view engine in app is set to pug
+  res.render('shop', { products: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
