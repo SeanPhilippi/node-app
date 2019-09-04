2f021f9836d71cdb972b5832ae8050edf16b4a74
Sean Philippi <sean.philippi@gmail.com>
2019-09-03 19:07:54 -0500

    converting to pug template engine

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,18 @@ const express = require('express');
 const rootDir = require('../utils/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware!');
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'product.html'));
   console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,13 @@ const express = require('express');
 const rootDir = require('../utils/path');
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
+  console.log('products', adminData.products);
   // path.join() is used vs concat because it detects the operating system the app is running on and adjusts for '/' or '\'
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = router;
