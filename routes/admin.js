6d38a6435f04eb65023df74e6f94e5220793c210
Sean Philippi <sean.philippi@gmail.com>
2019-09-16 09:13:42 -0500

    pug templating and using props

--- routes/admin.js
+++ routes/admin.js
@@ -5,12 +5,14 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   console.log('In another middleware!');
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { pageTitle: 'Add Product' });
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   products.push({ title: req.body.title });
